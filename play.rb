fd4f95323471a2a3b53b626111d590a1a37d60f1
John Nkp <john.nkp1@gmail.com>
2020-11-23 23:17:48 +1100

    first commit

---
+++ play.rb
@@ -0,0 +1,88 @@
+class Hangman
+    def initialize
+        # '@' means an instance variable, allows variable to be scoped within the class
+        # @letters = ("a".."z").to_a
+        # for correct guesses
+        @word = words.sample
+        @lives = 7
+        @word_teaser = ""
+
+        @word.first.size.times do
+            @word_teaser += "_ "
+        end
+    end
+
+    def words
+        [
+            ["cricket", "A game played by gentlement"],
+            ["jogging", "we are not walking..."],
+            ["celebrate", "Remebering special moments"],
+            ["continent", "There are 7 of these in the world"],
+            ["exotic", "Not from around here..."],
+        ]
+    end
+    
+    def print_teaser last_guess = nil
+        update_teaser(last_guess) unless last_guess.nil?
+        puts @word_teaser
+    end
+
+    def update_teaser last_guess
+        new_teaser = @word_teaser.split
+
+        # do is part of ruby loops, the beginning of the block of code to be repeatedly executed
+        new_teaser.each_with_index do |letter, index|  
+            # replace blank values with guessed letter of matches letter in word
+            if letter == '_' && @word.first[index] == last_guess
+                new_teaser[index] = last_guess
+            end
+        end
+
+        @word_teaser = new_teaser.join(" ")
+    end
+    
+    def make_guess
+        if @lives > 0
+            puts "Enter a letter"
+            guess = gets.chomp
+            
+            #if letter is not part of word then remove from letters array
+            good_guess = @word.first.include? guess
+
+            if guess == "exit"
+                puts "Thank you for playing!"
+            elsif good_guess
+                puts "You are correct"
+                
+                print_teaser guess
+
+                if @word.first == @word_teaser.split.join
+                    puts "Congratulations.. you have won this round!"
+                else
+                    make_guess
+                end
+            else
+                @lives -= 1
+                puts "Sorry... you have #{ @lives } lives left. Try again!"
+                make_guess
+            end  
+        else
+            puts "Game over!.... Better luck next time!"
+        end
+    end
+
+    def begin
+        # ask user for a letter
+        puts "New game started.. your word is #{@word.first.size} characters long" 
+        puts "Too exit game at any point type 'exit'"
+        print_teaser  
+
+        puts "Clue: #{ @word.last }"        
+
+        make_guess
+    end
+end
+
+game = Hangman.new
+game.begin
+
